0487312ed11fdc007bb46605b09acd2411eb2794
nhat.tran <nhattq.coding@gmail.com>
2021-05-20 07:52:10 +0700

    chore: move rollupTs to plugin vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,50 +1,34 @@
 import vue from '@vitejs/plugin-vue'
 import rollupTs from 'rollup-plugin-typescript2'
-import path from 'path'
 import type { ConfigEnv, UserConfig } from 'vite'
-
-const resolvePath = (p) => path.resolve(__dirname, p)
 
 // https://vitejs.dev/config/
 export default ({ command, mode }: ConfigEnv): UserConfig => ({
   plugins: [
     vue(),
-    // {
-    //   ...rollupTs({
-    //     check: true,
-    //     tsconfig: path.resolve(__dirname, `tsconfig.json`), // your tsconfig.json path
-    //     tsconfigOverride: {
-    //       compilerOptions: {
-    //         sourceMap: false,
-    //         declaration: true,
-    //         declarationMap: false,
-    //       },
-    //     },
-    //   }),
-    //   enforce: 'pre',
-    // },
+    {
+      ...rollupTs({
+        check: true,
+        tsconfig: 'tsconfig.json',
+        tsconfigOverride: {
+          compilerOptions: {
+            sourceMap: false,
+            declaration: true,
+            declarationMap: false,
+          },
+        },
+      }),
+      enforce: 'pre',
+    },
   ],
   build: {
     minify: mode === 'production',
     lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
+      entry: 'src/index.ts',
       name: 'AButton',
     },
     rollupOptions: {
-      input: resolvePath('src/index.ts'),
-      plugins: [
-        rollupTs({
-          check: true,
-          tsconfig: path.resolve(__dirname, `tsconfig.json`), // your tsconfig.json path
-          // tsconfigOverride: {
-          //   compilerOptions: {
-          //     sourceMap: false,
-          //     declaration: true,
-          //     declarationMap: false,
-          //   },
-          // },
-        }),
-      ],
+      input: 'src/index.ts',
       external: ['vue'],
       output: [
         {
